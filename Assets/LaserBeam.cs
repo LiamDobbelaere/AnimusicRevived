4a29a7964c63e03770cb06b111a42be8b2d5d60e
Timothy <timothymailacct@gmail.com>
2018-01-26 21:09:32 -0600

    Added a laser and a very -special- MIDI track

---
+++ Assets/LaserBeam.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam : Actor
+{
+    public Color32 On;
+    public Color32 Off;
+    public Animator LaserAnim;
+    public int MIDIChannel;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public override void NoteOnEvent(int channel, int key, int velocity)
+    {
+        if (channel == MIDIChannel && key != 40) {
+            GetComponent<Renderer>().material.SetColor("_Color", On);
+            float VelocityF = (float)(velocity) / 20;
+            LaserAnim.SetFloat("Speed", VelocityF);
+            Debug.Log("Velocity : " + velocity);
+        }
+    }
+    public override void NoteOffEvent(int channel, int key)
+    {
+        if (channel == MIDIChannel)
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", Off);
+            LaserAnim.SetFloat("Speed", 0);
+        }
+    }
+}
